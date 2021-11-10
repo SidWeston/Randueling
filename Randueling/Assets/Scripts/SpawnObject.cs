da2a0c833d1558c35b4c625b730fea822f56e773
NawrotPawel <71453503+NawrotPawel@users.noreply.github.com>
2021-11-10 13:27:54 +0000

    Assets Randomizer
    Added in the randomizer for assets, tested and working correctly, some objects are facing the wrong way, I believe this is just the variation in axis between the modeling program and Unity

---
+++ Randueling/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SpawnObject : MonoBehaviour
+{
+
+    public List<GameObject> prefabs = new List<GameObject>();
+
+    public Vector3 center;
+    public Vector3 size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObj();
+        SpawnObj();
+        SpawnObj();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            SpawnObj();
+
+
+    }
+
+    // Spawns objects at random into the designed cube in random order
+    public void SpawnObj() {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), (Random.Range(-size.y / 2, size.y / 2)), (Random.Range(-size.z / 2, size.z / 2)));
+
+        int randomprefab = Random.Range(0, prefabs.Count - 1);
+        Instantiate(prefabs[randomprefab], pos, Quaternion.identity);
+    }
+
+    // Creates red cube on screen to show area in which prefabs can spawn
+    private void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = new Color(1,0,0,0.5f);
+        Gizmos.DrawCube(transform.localPosition + center, size);
+    }
+}
