f5902052230e9217c8c0daa81abe9138632f557a
LudoWest <71764011+LudoWest@users.noreply.github.com>
2021-12-02 14:24:57 +0000

    Revert "random weapon selection"
    This reverts commit dbb3e97ff1ed94cb58efb73516b0d4b282a36d00.

--- Randueling/Assets/Scripts/RandomWeaponSelection.cs
+++ Randueling/Assets/Scripts/RandomWeaponSelection.cs
@@ -9,43 +9,17 @@ public class RandomWeaponSelection : MonoBehaviour
     public GameObject playerTwo;
 
     public List<GameObject> weaponsList;
-    public List<GameObject> playerOneWeapons;
-    public List<GameObject> playerTwoWeapons;
-
 
     // Start is called before the first frame update
     void Start()
     {
         playerOne = GameObject.FindGameObjectWithTag("PlayerOne");
         playerTwo = GameObject.FindGameObjectWithTag("PlayerTwo");
-
-        GenerateRandomWeapons();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerOne == null)
-        {
-            playerOne = GameObject.FindGameObjectWithTag("PlayerOne");
-        }
-        if(playerTwo == null)
-        {
-            playerTwo = GameObject.FindGameObjectWithTag("PlayerTwo");
-        }
+        
     }
-
-    public void GenerateRandomWeapons()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            playerOneWeapons.Add(weaponsList[Random.Range(0, weaponsList.Count)]);
-        }
-        for (int i = 0; i < 3; i++)
-        {
-            playerTwoWeapons.Add(weaponsList[Random.Range(0, weaponsList.Count)]);
-        }
-    }
-
 }
